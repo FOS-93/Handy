d348241c358a554efeebaa865be0a3b1b6eafc81
Sergio Sosa Romero <smatias.sosa@gmail.com>
2022-06-23 20:48:30 -0300

    creados handymen y appointments

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_one :handyman
   has_many :appointments
   has_many :reviews
+
+  before_save :set_full_name
+
+  private
+
+  def set_full_name
+    self.full_name = "#{self.first_name} #{self.last_name}".strip
+  end
 end
