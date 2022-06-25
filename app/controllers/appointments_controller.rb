817db26c28e9644a27911ce1eb94ce153ef19086
Sergio Sosa Romero <smatias.sosa@gmail.com>
2022-06-25 13:59:21 -0300

    controlador y un par de vistas

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,27 @@
 class AppointmentsController < ApplicationController
 
+  def new
+    # we need @user in our `simple_form_for`
+    @handyman = Handyman.find(params[:handyman_id])
+    @appointment = Appointment.new
+  end
 
+  def create
+    @appointment = Appointment.new(appointment_params)
+    # we need `user_id` to associate appointment with corresponding user
+    @handyman = Handyman.find(params[:handyman_id])
+    @appointment.user = current_user
+    @appointment.handyman = @handyman
+    if @appointment.save
+      redirect_to my_profile_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:date, :description, :status, :skill)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users do
-    resources :handymen, only: %i[new create]
+  resources :handymen do
+    resources :skills, only: %i[new create]
+    resources :appointments, only: %i[new create]
   end
 
-  resources :handymen, except: %i[new create] do
-    resources :skills, only: %i[new create]
-  end
 
   get "/my_profile", to: "pages#my_profile", as: :my_profile
 
