e24da22536bb5e1d5a342417a9d5b59e8f074fba
alan vergara <alan_vergara_p@hotmail.com>
2022-06-30 21:02:42 -0500

    add some features to appointments and profile

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :handyman
   has_many :reviews
+  validates :date, presence: true
+  validates :description, presence: true
+  validates :skill, presence: true
 end
