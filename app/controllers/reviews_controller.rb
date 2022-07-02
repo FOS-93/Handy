3b4582df7f95aa355c99449bf42069906b5e3bc8
Sergio Sosa Romero <smatias.sosa@gmail.com>
2022-07-02 14:18:48 -0300

    separamos los reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,12 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @appointment = Appointment.find(params[:appointment_id])
+    @handyman = @appointment.handyman
+    @user = @appointment.user
     @review.appointment = @appointment
+    @review.handyman = @handyman
+    @review.user = @user
+    @review.author = current_user.email
     if @review.save
       redirect_to my_profile_path
     else
